a838f52119e4a1617c64eb2bedf8871680ef0dac
lisandrofernando <lisandrusfernandus@gmail.com>
2025-12-23 15:19:08 -0600

    Ignoring secrets

--- Modules/dev/variables.tf
+++ Modules/dev/variables.tf
@@ -78,5 +78,27 @@ variable "admin_password" {
   sensitive   = true
 }
 
+variable "client_id" {
+  description = "Azure service principal client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "client_secret" {
+  description = "Azure service principal client secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
 
 
--- Modules/qa/variables.tf
+++ Modules/qa/variables.tf
@@ -77,3 +77,25 @@ variable "admin_password" {
   sensitive   = true
 }
 
+variable "client_id" {
+  description = "Azure service principal client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "client_secret" {
+  description = "Azure service principal client secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
