36f480ee61bc32e9c186357f43c14983598413b3
lisandrofernando <lisandrusfernandus@gmail.com>
2025-12-27 21:58:52 -0600

    commenting hardcoded values

--- Modules/prod/variables.tf
+++ Modules/prod/variables.tf
@@ -77,24 +77,25 @@ variable "admin_password" {
   sensitive   = true
 }
 
-variable "client_id" {
-  description = "Azure service principal client ID"
-  type        = string
-  sensitive   = true
-}
+# Provider authentication variables are provided via credentials.auto.tfvars
+# variable "client_id" {
+#   description = "Azure service principal client ID"
+#   type        = string
+#   sensitive   = true
+# }
 
-variable "client_secret" {
-  description = "Azure service principal client secret"
-  type        = string
-  sensitive   = true
-}
+# variable "client_secret" {
+#   description = "Azure service principal client secret"
+#   type        = string
+#   sensitive   = true
+# }
 
-variable "tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-}
+# variable "tenant_id" {
+#   description = "Azure tenant ID"
+#   type        = string
+# }
 
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-}
+# variable "subscription_id" {
+#   description = "Azure subscription ID"
+#   type        = string
+# }
